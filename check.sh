b900499d24dd4db910c9d88d7a699459ffcdaec2
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-06 12:20:48 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -17,9 +17,9 @@ else
     docker rm -f $(docker ps -aq) && docker rmi -f $(docker images -q) 
     yes | docker system prune -a
     echo "DOWNLOAD FILES FOR Linux"
-    rm -rf $binary_name && rm -rf ionet_device_cache.txt
-    curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_name -o $binary_name
-    chmod +x $binary_name
+    rm -rf $file_path/$binary_name && rm -rf $file_path/ionet_device_cache.txt
+    curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_name -o $file_path/$binary_name
+    chmod +x $file_path/$binary_name
     echo "START NEW NODE"
     cd "$file_path" && $launch_string
 fi
