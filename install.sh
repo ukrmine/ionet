f92bb903ffdd4926f63551d9183cb924da849482
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-10 13:36:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ vmname="ionet"
 password="Password"
 homedir="/home"
 ssd="48G"
+IP_ADDR="192.168.122.10"
 
 # Function to select CPU type
 select_cpu_type() {
@@ -55,14 +56,16 @@ select_cpu_type() {
 select_variables() {
     read -p "Enter virtual host name (default: $vmhost): " vmhost_input
     vmhost="${vmhost_input:-$vmhost}"
-    read -p "Enter Login (default: $vmname): " vmname_input
+    read -p "Enter login (default: $vmname): " vmname_input
     vmname="${vmname_input:-$vmname}"
     read -p "Enter password (default: $password): " password_input
     password="${password_input:-$password}"
     read -p "Enter home directory (default: $homedir): " homedir_input
     homedir="${homedir_input:-$homedir}"
-    read -p "Enter Disk size (default: $ssd): " ssd_input
+    read -p "Enter SSD size (default: $ssd): " ssd_input
     ssd="${ssd_input:-$ssd}"
+    read -p "Enter IP address (default: $IP_ADDR): " IP_ADDR_input
+    IP_ADDR="${IP_ADDR_input:-$IP_ADDR}"
 }
 
 select_cpu_type
@@ -71,10 +74,11 @@ select_variables
 # Output selected CPU type and other variables
 echo "Selected CPU type: $cpu_type"
 echo "Virtual host name: $vmhost"
-echo "Login: $vmname"
+echo "Virtual machine name: $vmname"
 echo "Password: $password"
 echo "Home directory: $homedir"
-echo "Disk size: $ssd"
+echo "SSD size: $ssd"
+echo "IP address: $IP_ADDR"
 
 basedir=$homedir/base
 vmdir=$homedir/$vmname
@@ -87,13 +91,25 @@ sudo apt install qemu-kvm libvirt-daemon-system virt-manager bridge-utils cloud-
 sudo usermod -aG kvm $USER
 sudo usermod -aG libvirt $USER
 mkdir -p $basedir $vmdir
-wget -P "$basedir" https://cloud-images.ubuntu.com/focal/current/$image
+if [ ! -f "$basedir/$image" ]; then
+  wget -P "$basedir" https://cloud-images.ubuntu.com/focal/current/$image
+fi
 qemu-img create -F qcow2 -b $basedir/$image -f qcow2 $vmdir/$vmname.qcow2 $ssd
-virsh net-start default
-
+if [[ -z "virsh net-list --all | grep "default\s*active"" ]]; then
+    echo "Network 'default' is not active. Starting the network..."
+    virsh net-start default
+else
+    echo "Network 'default' is active."
+fi
+sudo -u root ssh-keygen -t rsa -b 2048 -f "/root/.ssh/id_rsa" -N ""
+ssh_key=$(cat /root/.ssh/id_rsa.pub)
+echo "alias noda='ssh root@$IP_ADDR'" >> /root/.bashrc
+echo "alias nodarerun='ssh root@$IP_ADDR '/root/rerun.sh''" >> /root/.bashrc
+#chmod a+x ~/.bashrc
+#PS1='$ '
+. ~/.bashrc
 MAC_ADDR=$(printf '52:54:00:%02x:%02x:%02x' $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
 INTERFACE=eth01
-IP_ADDR=192.168.122.10
 
 cat >$vmdir/network-config <<EOF
 ethernets:
@@ -117,18 +133,32 @@ cat >$vmdir/user-data <<EOF
 hostname: $vmhost
 manage_etc_hosts: true
 users:
-  - name: $vmname
-    sudo: ALL=(ALL) NOPASSWD:ALL
-    groups: users, admin
-    home: /home/$vmname
+  - name: root
     shell: /bin/bash
-    lock_passwd: false
+    lock-passwd: false
+    ssh-authorized-keys:
+      - $ssh_key
 ssh_pwauth: true
 disable_root: false
 chpasswd:
   list: |
     $vmname:$password
+    root:$password
   expire: false
+write_files:
+  - path: /root/script.sh
+    permissions: '0755'
+    content: |
+      #!/bin/bash
+      sed -i "s/#Port 22/Port 22/" /etc/ssh/sshd_config
+      sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+      sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+      curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+      apt install -y speedtest
+runcmd:
+  - [ bash, "/root/script.sh" ]
+  - service ssh reload
+  - rm /root/script.sh
 EOF
 
 touch $vmdir/meta-data
