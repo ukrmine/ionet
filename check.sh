2a8b25ecb793e29904428326b914283efcaf2346
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-10 20:14:04 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,19 +2,34 @@
 launch_string="Yours Run Docker Command"
 file_path="/root"
 binary_name=$(basename "${launch_string%% *}")
+MonID=$(docker ps -a | grep "io-worker-monitor" | awk '{print $1}')
+MonCPU=$(docker stats --no-stream $MonID --format "{{.CPUPerc}}" | tr -d '%')
+
 if docker ps -a --format '{{.Image}}' | grep -q "io-launch"; then
     echo "io-launch is WORKING, wait 5min"
     sleep 300
     if docker ps -a --format '{{.Image}}' | grep -q "io-launch"; then
         echo "io-launch still WORKING, STOP ALL CONTAINERS"
-        docker rm -f $(docker ps -aq) 
+        action="RESTART"
     fi
 fi
+
 if [[ $(docker ps | grep -c "io-worker-monitor") -eq 1 && $(docker ps | grep -c "io-worker-vc") -eq 1 ]]; then
-    echo "NODE IS WORKING"
+    if [[ $(echo "$MonCPU >= 0.0" | bc -l) -eq 1 ]]; then
+        echo "Containers is OK"
+        echo "Monitor CPU usage $MonCPU%"
+    else
+	echo "Monitor not use CPU $MonCPU%"
+	action="RESTART"
+    fi
 else
+    echo "Containers are broken"
+    action="RESTART"
+fi
+
+if [[ "$action" == "RESTART" ]]; then
     echo "STOP AND DELETE ALL CONTAINERS"
-    docker rm -f $(docker ps -aq) && docker rmi -f $(docker images -q) 
+    docker rm -f $(docker ps -aq) && docker rmi -f $(docker images -q)
     yes | docker system prune -a
     echo "DOWNLOAD FILE $binary_name"
     rm -rf $file_path/$binary_name && rm -rf $file_path/ionet_device_cache.txt
