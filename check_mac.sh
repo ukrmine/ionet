8d481f6497ac62b409c6c9848b7af4880d88a50a
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-21 15:59:31 +0200

    Create check_mac.sh

---
+++ check_mac.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir ~/Documents/ionet && cd ~/Documents/ionet
+curl -L -o ~/Documents/ionet/check.sh https://github.com/ukrmine/ionet/raw/main/check.sh
+chmod +x ~/Documents/ionet/check.sh
+read -p "Yours Run Docker Command: " launch_string
+sed -i '' "2s/.*/launch_string=\"$launch_string\"/" ~/Documents/ionet/check.sh
+crontab<<EOF
+*/10 * * * * ~/Documents/ionet/check.sh
+EOF
