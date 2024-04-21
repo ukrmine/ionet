8eb825d600db4caeeb67dd101f20f3475a7ae0df
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-21 17:54:41 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -33,8 +33,8 @@ if [[ "$action" == "RESTART" ]]; then
     yes | docker system prune -a
     echo "DOWNLOAD FILE $binary_name"
     rm -rf $file_path/$binary_name && rm -rf $file_path/ionet_device_cache.txt
-    curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_name -o "$file_path/$binary_name"
-    chmod +x "$file_path/$binary_name"
+    curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_name -o $file_path/$binary_name
+    chmod +x $file_path/$binary_name
     echo "START NEW NODE"
     $file_path/$launch_string
 fi
