54b35ccfe280d293c232252be5c91802db3729ad
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-21 17:40:34 +0200

    Update check_mac.sh

--- check_mac.sh
+++ check_mac.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-curl -L -o ~/Documents/ionet/check.sh https://github.com/ukrmine/ionet/raw/main/check.sh
-chmod +x ~/Documents/ionet/check.sh
+home_dir="$HOME/Documents/ionet"
+curl -L -o $home_dir/check.sh https://github.com/ukrmine/ionet/raw/main/check.sh
+chmod +x $home_dir/check.sh
 read -p "Yours Run Docker Command: " new_string
-sed -i '' "s|launch_string=\"Yours Run Docker Command\"|launch_string=\"$new_string\"|" ~/Documents/ionet/check.sh
-sed -i '' "s|file_path=\"/root\"|file_path=\"~/Documents/ionet\"|" ~/Documents/ionet/check.sh
+sed -i '' "s|launch_string=\"Yours Run Docker Command\"|launch_string=\"$new_string\"|" $home_dir/check.sh
+sed -i '' "s|file_path=\"/root\"|file_path=\"$home_dir\"|" $home_dir/check.sh
 crontab<<EOF
-*/10 * * * * ~/Documents/ionet/check.sh
+*/10 * * * * $home_dir/check.sh
 EOF
-rm ~/Documents/ionet/check_mac.sh
+rm $home_dir/check_mac.sh
