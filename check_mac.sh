4f1a7392669ef2cc1019bef5ba6531aeccdb53c9
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-24 13:21:47 +0200

    Update check_mac.sh

--- check_mac.sh
+++ check_mac.sh
@@ -5,6 +5,7 @@ chmod +x $home_dir/check.sh
 read -p "Yours Run Docker Command: " new_string
 sed -i '' "s|launch_string=\"Yours Run Docker Command\"|launch_string=\"$new_string\"|" $home_dir/check.sh
 sed -i '' "s|file_path=\"/root\"|file_path=\"$home_dir\"|" $home_dir/check.sh
+sed -i '' "s|#colima start|colima start|" $home_dir/check.sh
 crontab<<EOF
 */10 * * * * $home_dir/check.sh
 EOF
