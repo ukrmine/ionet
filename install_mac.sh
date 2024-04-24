2897a67a7cbf6e836154d1f07593c959e4dc6c5b
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-24 13:22:35 +0200

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -22,15 +22,12 @@ if ! command -v docker &> /dev/null; then
 else
     echo "Docker is already installed."
 fi
-#curl -L -o $home_dir/Docker.dmg https://desktop.docker.com/mac/main/arm64/145265/Docker.dmg
-#sudo hdiutil attach Docker.dmg
-#sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --accept-license
-#sudo hdiutil detach /Volumes/Docker
 curl -L -o $home_dir/check.sh https://github.com/ukrmine/ionet/raw/main/check.sh
 chmod +x $home_dir/check.sh
 read -p "Yours Run Docker Command: " new_string
 sed -i '' "s|launch_string=\"Yours Run Docker Command\"|launch_string=\"$new_string\"|" $home_dir/check.sh
 sed -i '' "s|file_path=\"/root\"|file_path=\"$home_dir\"|" $home_dir/check.sh
+sed -i '' "s|#colima start|colima start|" $home_dir/check.sh
 crontab<<EOF
 */10 * * * * $home_dir/check.sh
 EOF
