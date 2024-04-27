514d7dc6581bee975e1636963ae272837aa1fc4f
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-27 14:39:13 +0200

    Update check1.sh

--- check1.sh
+++ check1.sh
@@ -4,6 +4,7 @@ file_path="/root"
 binary_name=$(basename "${launch_string%% *}")
 echo "$file_path/$binary_name"
 #colima start
+token=$(awk -F'"' '{print $36}' ionet_device_cache.json)
 MonID=$(docker ps -a | grep "io-worker-monitor" | awk '{print $1}')
 MonCPU=$(docker stats --no-stream $MonID --format "{{.CPUPerc}}" | tr -d '%')
 
@@ -34,9 +35,9 @@ if [[ "$action" == "RESTART" ]]; then
     docker rm -f $(docker ps -aq) && docker rmi -f $(docker images -q)
     yes | docker system prune -a
     echo "DOWNLOAD FILE $binary_name"
-    rm -rf $file_path/$binary_name && rm -rf $file_path/ionet_device_cache.txt
+    rm -rf $file_path/$binary_name
     curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_name -o $file_path/$binary_name
     chmod +x $file_path/$binary_name
     echo "START NEW NODE"
-    $file_path/$launch_string
+    echo "Yes" | $file_path/$launch_string --token "$token"
 fi
