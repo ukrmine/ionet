45fce9cb1661ee499e7982835c10115dfb07a00c
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-29 09:38:36 +0200

    Update install_mac1.sh

--- install_mac1.sh
+++ install_mac1.sh
@@ -6,7 +6,7 @@ else
     echo "It is recommended to update your Mac to the latest version of Sonoma"
 fi
 home_dir="$HOME/Documents/ionet"
-cd $home_dir
+mkdir $home_dir && cd $home_dir
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Install it via Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -22,15 +22,15 @@ if ! command -v docker &> /dev/null; then
 else
     echo "Docker is already installed."
 fi
-curl -L -o $home_dir/check.sh https://github.com/ukrmine/ionet/raw/main/check.sh
-chmod +x $home_dir/check.sh
-read -p "Yours Run Docker Command: " new_string
-sed -i '' "s|launch_string=\"Yours Run Docker Command\"|launch_string=\"$new_string\"|" $home_dir/check.sh
+curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/io_net_launch_binary_mac -o $home_dir/io_net_launch_binary_mac
+chmod +x $home_dir/io_net_launch_binary_mac
+curl -L -o $home_dir/check.sh https://github.com/ukrmine/ionet/raw/main/check.sh && chmod +x $home_dir/check.sh
+#read -p "Yours Run Docker Command: " new_string
 sed -i '' "s|file_path=\"/root\"|file_path=\"$home_dir\"|" $home_dir/check.sh
 sed -i '' "s|#colima start|colima start|" $home_dir/check.sh
 crontab<<EOF
-*/10 * * * * $home_dir/check.sh
+*/12 * * * * $home_dir/check.sh
 EOF
 rm $home_dir/install_mac.sh
 softwareupdate --install-rosetta --agree-to-license
-$home_dir/check.sh
+echo "Run the command to connect device (worker) from https://cloud.io.net/worker/devices/"
