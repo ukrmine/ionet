a54303886b6362712cdbf29388aea36be2d7f2a8
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-29 19:54:42 +0200

    Update install_mac1.sh

--- install_mac1.sh
+++ install_mac1.sh
@@ -99,7 +99,7 @@ sed -i '' "s|#colima start|colima start|" $home_dir/check.sh
 #$new_string
 #echo "Yes" | $launch_string
 #read -p "Paste your token for silent authentification" token
-output=$(echo "Yes" | $launch_string | grep "Option 1")
+output=$(echo "Yes" | $launch_string | tee /dev/tty)
 token=$(echo "$output" | grep "Use the following token as" | awk '{print $NF}')
 echo "Yes" | "$launch_string --token="$token""
 echo "Wait until the containers are loaded for 10min."
