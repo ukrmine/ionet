f3af896a23564b48b382b2bfd8bde2d66bc16df9
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-29 22:55:20 +0200

    Update install_mac1.sh

--- install_mac1.sh
+++ install_mac1.sh
@@ -25,46 +25,39 @@ chmod +x $home_dir/$binary_name
 curl -L -o $home_dir/check.sh https://github.com/ukrmine/ionet/raw/main/check.sh && chmod +x $home_dir/check.sh
 sed -i '' "s|^file_path=.*|file_path=\"$home_dir\"|g" $home_dir/check.sh
 sed -i '' "s|#colima start|colima start|" $home_dir/check.sh
+
+install_docker() {
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew install --cask docker
+brew install docker docker-compose colima
+colima start
+unset DOCKER_HOST
+unset DOCKER_CERT_PATH
+unset DOCKER_TLS_VERIFY
+echo "Docker is successfully installed."
+}
+
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Install it via Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    brew install --cask docker
-    brew install docker docker-compose colima
-    colima start
-    unset DOCKER_HOST
-    unset DOCKER_CERT_PATH
-    unset DOCKER_TLS_VERIFY
-    echo "Docker is successfully installed."
+    install_docker
 else
     echo "Docker is already installed."
 fi
-cache_file="ionet_device_cache"
-new_string=""
-if [ -f "$cache_file.json" ]; then
-    echo "Configuration file found."
-    $home_dir/check.sh
-else
-    if [ -f "$cache_file.txt" ]; then
-        echo "Old worker data found."
-        json_data=$(cat ionet_device_cache.txt)
-        device_name=$(echo "$json_data" | awk -F', ' '{print $1}' | awk -F': ' '{print $2}' | tr -d '"')
-        device_id=$(echo "$json_data" | awk -F', ' '{print $2}' | awk -F': ' '{print $2}' | tr -d '"')
-        user_id=$(echo "$json_data" | awk -F', ' '{print $3}' | awk -F': ' '{print $2}' | tr -d '"')
-        operating_system=$(echo "$json_data" | awk -F', ' '{print $4}' | awk -F': ' '{print $2}' | tr -d '"')
-        usegpus=$(echo "$json_data" | awk -F', ' '{print $5}' | awk -F': ' '{print $2}' | tr -d '"}')
-    else
-        echo "No worker data found. Install a new worker."
-        echo "Guide to launching a worker https://link.medium.com/vnbuHZ3kaJb - 1.3 command from this article"
-        read -p "Run the command to connect device (worker) from https://cloud.io.net/worker/devices/ : " new_string
-    fi
-fi
 
+cache_file="ionet_device_cache"
+
+install_without_token() {
+json_data=$(cat ionet_device_cache.txt)
+device_name=$(echo "$json_data" | awk -F', ' '{print $1}' | awk -F': ' '{print $2}' | tr -d '"')
+device_id=$(echo "$json_data" | awk -F', ' '{print $2}' | awk -F': ' '{print $2}' | tr -d '"')
+user_id=$(echo "$json_data" | awk -F', ' '{print $3}' | awk -F': ' '{print $2}' | tr -d '"')
+operating_system=$(echo "$json_data" | awk -F', ' '{print $4}' | awk -F': ' '{print $2}' | tr -d '"')
+usegpus=$(echo "$json_data" | awk -F', ' '{print $5}' | awk -F': ' '{print $2}' | tr -d '"}')
 echo "Device Name: $device_name"
 echo "Device ID: $device_id"
 echo "User ID: $user_id"
-
 case $operating_system in
     "macOS")
         binary_name="io_net_launch_binary_mac"
@@ -80,8 +73,34 @@ case $operating_system in
         exit 1
         ;;
 esac
+}
 
-if [ -z "$new_string" ]; then
+new_string=""
+
+if [ -f "$cache_file.json" ]; then
+    json_data=$(cat $cache_file.json)
+    token=$(echo "$json_data" | awk -F',' '{print $9}' | awk -F':' '{print $2}' | tr -d '"')
+    if [[ -n $token ]]; then
+        echo "Configuration file found with token. Run worker"
+        $home_dir/check.sh
+        exit 1
+    else
+        echo "Configuration file found without token."
+        install_without_token
+    fi
+else
+    if [ -f "$cache_file.txt" ]; then
+        echo "Old worker data found."
+        install_without_token
+    else
+        echo "No worker data found. Install a new worker."
+        echo "Guide to launching a worker. Insert the command 1.3 from article https://link.medium.com/vnbuHZ3kaJb"
+        read -p "Run the command to connect device (worker) from https://cloud.io.net/worker/devices/ : " new_string
+        
+    fi
+fi
+
+if [[ -n $new_string ]]; then
     launch_string="$binary_name --device_id="$device_id" --user_id="$user_id" --operating_system="$operating_system" --usegpus="$usegpus" --device_name="$device_name"" 
 else
     launch_string=${new_string#./}
