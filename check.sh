9bb4dc43b4f3af3aad2c41e561ce452ee1cba8f8
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-30 00:20:22 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 file_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#colima start
 
 if [ -f "$file_path/ionet_device_cache.json" ]; then
     json_data=$(cat $file_path/ionet_device_cache.json)
     token=$(echo "$json_data" | awk -F',' '{print $9}' | awk -F':' '{print $2}' | tr -d '"')
     if [[ -n $token ]]; then
         echo "Worker data found with token. Run worker"
-        device_file="ionet_device_cache.json"
+        
     else
-        echo "Worker data found without token."
+        echo "Worker data found without token. Add web token"
         exit 1
     fi
 else
-    echo "Configuration file not found."
+    echo "Configuration file not found. Install worker"
     exit 1
 fi
+
+get_data() {
 device_name=$(echo "$json_data" | awk -F', ' '{print $1}' | awk -F': ' '{print $2}' | tr -d '"')
 device_id=$(echo "$json_data" | awk -F', ' '{print $2}' | awk -F': ' '{print $2}' | tr -d '"')
 user_id=$(echo "$json_data" | awk -F', ' '{print $3}' | awk -F': ' '{print $2}' | tr -d '"')
@@ -39,45 +42,50 @@ case $operating_system in
         ;;
 esac
 
-#colima start
-MonID=$(docker ps -a | grep "io-worker-monitor" | awk '{print $1}')
-MonCPU=$(docker stats --no-stream $MonID --format "{{.CPUPerc}}" | tr -d '%')
 launch_string="./$binary_name --device_id="$device_id" --user_id="$user_id" --operating_system="$operating_system" --usegpus="$usegpus" --device_name="$device_name""
+}
 
-if [[ "$1" == "-r" ]]; then
-    action="RESTART"
-else
-    if docker ps -a --format '{{.Image}}' | grep -q "io-launch"; then
-        echo "io-launch container is WORKING, wait 5min"
-        sleep 300
-        if docker ps -a --format '{{.Image}}' | grep -q "io-launch"; then
-            echo "io-launch container still WORKING, STOP ALL CONTAINERS"
-            action="RESTART"
-        fi
-    fi
-
-    if [[ $(docker ps | grep -c "io-worker-monitor") -eq 1 && $(docker ps | grep -c "io-worker-vc") -eq 1 ]]; then
-        if [[ $(echo "$MonCPU >= 0.0" | bc -l) -eq 1 ]]; then
-            echo "Containers is OK"
-            echo "Monitor CPU usage $MonCPU%"
-        else
-            echo "Monitor not use CPU $MonCPU%"
-            action="RESTART"
-        fi
+    
+check_containers() {
+if [[ $(docker ps | grep -c "io-worker-monitor") -eq 1 && $(docker ps | grep -c "io-worker-vc") -eq 1 ]]; then
+    MonID=$(docker ps -a | grep "io-worker-monitor" | awk '{print $1}')
+    MonCPU=$(docker stats --no-stream $MonID --format "{{.CPUPerc}}" | tr -d '%')
+    if [[ $(echo "$MonCPU >= 0.0" | bc -l) -eq 1 ]]; then
+        echo "Containers is OK"
+        echo "Monitor CPU usage $MonCPU%"
     else
-        echo "Containers are broken"
-        action="RESTART"
+        echo "Monitor not use CPU $MonCPU%"
+        restart
+    fi
+else
+    echo "Containers are broken"
+    restart
+fi
+}
+
+check_io_launch() {
+if docker ps -a --format '{{.Image}}' | grep -q "io-launch"; then
+    echo "io-launch container is WORKING, wait 5min"
+    sleep 300
+    if docker ps -a --format '{{.Image}}' | grep -q "io-launch"; then
+        echo "io-launch container still WORKING, STOP ALL CONTAINERS"
+        restart
     fi
 fi
+}
 
-if [[ "$action" == "RESTART" ]]; then
-    echo "STOP AND DELETE ALL CONTAINERS"
-    docker rm -f $(docker ps -aq) && docker rmi -f $(docker images -q)
-    yes | docker system prune -a
-    echo "DOWNLOAD FILE $binary_name"
-    rm -rf $file_path/$binary_name
-    curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_name -o $file_path/$binary_name
-    chmod +x $file_path/$binary_name
-    echo "START NEW NODE"
-    echo "Yes" | $file_path/$launch_string
+restart() {
+echo "STOP AND DELETE ALL CONTAINERS"
+docker rm -f $(docker ps -aq) && docker rmi -f $(docker images -q)
+yes | docker system prune -a
+echo "DOWNLOAD FILE $binary_name"
+rm -rf $file_path/$binary_name
+curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_name -o $file_path/$binary_name
+chmod +x $file_path/$binary_name
+echo "START NEW NODE"
+echo "Yes" | $file_path/$launch_string
+}
+
+if [[ "$1" == "-r" ]]; then
+    restart
 fi
