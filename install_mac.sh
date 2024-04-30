f398995c26dacacd37fb3a8ca53eca679ef304cf
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-04-30 16:55:24 +0200

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -20,11 +20,7 @@ else
     echo "The folder $home_dir already exists."
 fi
 cd $home_dir
-curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_name -o $home_dir/$binary_name
-chmod +x $home_dir/$binary_name
-curl -L -o $home_dir/check.sh https://github.com/ukrmine/ionet/raw/main/check.sh && chmod +x $home_dir/check.sh
-sed -i '' "s|^file_path=.*|file_path=\"$home_dir\"|g" $home_dir/check.sh
-sed -i '' "s|#colima start|colima start|" $home_dir/check.sh
+
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Install it via Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -40,22 +36,10 @@ if ! command -v docker &> /dev/null; then
 else
     echo "Docker is already installed."
 fi
-cache_file="ionet_device_cache"
-new_string=""
-if [ -f "$cache_file.json" ]; then
-    echo "Configuration file found."
-    $home_dir/check.sh
-else
-    if [ -f "$cache_file.txt" ]; then
-        echo "Old worker data found."
-        json_data=$(cat ionet_device_cache.txt)
-    else
-        echo "No worker data found. Install a new worker."
-        echo "Guide to launching a worker https://link.medium.com/vnbuHZ3kaJb - 1.3 command from this article"
-        read -p "Run the command to connect device (worker) from https://cloud.io.net/worker/devices/ : " new_string
-    fi
-fi
 
+cache_file="$home_dir/ionet_device_cache"
+
+install_without_token() {
 device_name=$(echo "$json_data" | awk -F', ' '{print $1}' | awk -F': ' '{print $2}' | tr -d '"')
 device_id=$(echo "$json_data" | awk -F', ' '{print $2}' | awk -F': ' '{print $2}' | tr -d '"')
 user_id=$(echo "$json_data" | awk -F', ' '{print $3}' | awk -F': ' '{print $2}' | tr -d '"')
@@ -64,7 +48,6 @@ usegpus=$(echo "$json_data" | awk -F', ' '{print $5}' | awk -F': ' '{print $2}' 
 echo "Device Name: $device_name"
 echo "Device ID: $device_id"
 echo "User ID: $user_id"
-
 case $operating_system in
     "macOS")
         binary_name="io_net_launch_binary_mac"
@@ -80,22 +63,65 @@ case $operating_system in
         exit 1
         ;;
 esac
+}
 
-if [ -z "$new_string" ]; then
-    launch_string="$binary_name --device_id="$device_id" --user_id="$user_id" --operating_system="$operating_system" --usegpus="$usegpus" --device_name="$device_name"" 
+new_install() {
+echo "Guide to launching a worker. Insert the command 1.3 from article https://link.medium.com/vnbuHZ3kaJb"
+read -p "Run the command to connect device (worker) from https://cloud.io.net/worker/devices/ : " new_string
+binary_name=$(basename "${new_string%% *}")
+device_name=$(echo "$new_string" | awk -F'[ =]' '{print $11}')
+device_id=$(echo "$new_string" | awk -F'[ =]' '{print $3}')
+user_id=$(echo "$new_string" | awk -F'[ =]' '{print $5}')
+operating_system=$(echo "$new_string" | awk -F'[ =]' '{print $7}')
+usegpus=$(echo "$new_string" | awk -F'[ =]' '{print $9}')
+echo "Device Name: $device_name"
+echo "Device ID: $device_id"
+echo "User ID: $user_id"
+}
+
+autorun() {
+crontab<<EOF
+*/12 * * * * $home_dir/check.sh
+EOF
+}
+
+curl -L -o $home_dir/check.sh https://github.com/ukrmine/ionet/raw/main/check.sh && chmod +x $home_dir/check.sh
+sed -i '' "s|^file_path=.*|file_path=\"$home_dir\"|g" $home_dir/check.sh
+sed -i '' "s|#colima start|colima start|" $home_dir/check.sh
+
+if [ -f "$cache_file.json" ]; then
+    json_data=$(cat $cache_file.json)
+    token=$(echo "$json_data" | awk -F',' '{print $9}' | awk -F':' '{print $2}' | tr -d '"')
+    if [[ -n $token ]]; then
+        echo "Worker data found with token. Run worker"
+        $home_dir/check.sh
+        exit 1
+    else
+        echo "Worker data found without token."
+        json_data=$(cat $cache_file.json)
+        install_without_token
+    fi
 else
-    launch_string=${new_string#./}
+    if [ -f "$cache_file.txt" ]; then
+        echo "Old worker data found."
+        json_data=$(cat $cache_file.txt)
+        install_without_token
+    else
+        echo "No worker data found. Install a new worker."
+        new_install
+    fi
 fi
 
-#crontab<<EOF
-#*/12 * * * * $home_dir/check.sh
-#EOF
-#rm $home_dir/install_mac.sh
-#softwareupdate --install-rosetta --agree-to-license
+curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_name -o $home_dir/$binary_name
+chmod +x $home_dir/$binary_name
+launch_string="$binary_name --device_id="$device_id" --user_id="$user_id" --operating_system="$operating_system" --usegpus="$usegpus" --device_name="$device_name"" 
+softwareupdate --install-rosetta --agree-to-license
 output=$(echo "Yes" | $home_dir/$launch_string | tee /dev/tty)
 token=$(echo "$output" | grep "Use the following token as" | awk '{print $NF}')
 sed -i '' 's/\("token":\)""/\1"'$token'"/' ionet_device_cache.json
 echo "Wait until the containers are loaded for 10min."
 sleep 420
 $home_dir/check.sh
+autorun
+#rm $home_dir/install_mac.sh
 echo "Congratulation. Your IO worker is launched and ready."
