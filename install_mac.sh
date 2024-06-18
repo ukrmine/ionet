391016c44bd651ad76cd90efca35596a3b0a9d9d
UkrMine <161887951+ukrmine@users.noreply.github.com>
2024-06-18 22:01:44 +0200

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -116,8 +116,10 @@ curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/$binary_na
 chmod +x $home_dir/$binary_name
 echo "Disable sleep mode for a device"
 $home_dir/$binary_name --disable_sleep_mode=true
-launch_string="$binary_name --device_id="$device_id" --user_id="$user_id" --operating_system="$operating_system" --usegpus="$usegpus" --device_name="$device_name"" 
+echo "Install Rosetta"
 softwareupdate --install-rosetta --agree-to-license
+echo "Run worker"
+launch_string="$binary_name --device_id="$device_id" --user_id="$user_id" --operating_system="$operating_system" --usegpus="$usegpus" --device_name="$device_name"" 
 output=$(echo "Yes" | $home_dir/$launch_string | tee /dev/tty)
 token=$(echo "$output" | grep "Use the following token as" | awk '{print $NF}')
 sed -i '' 's/\("token":\)""/\1"'$token'"/' ionet_device_cache.json
